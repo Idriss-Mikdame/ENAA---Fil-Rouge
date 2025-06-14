616b3b0a55bbb14c6bc0da772b8ad8348722cbe9
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-06-14 23:05:04 +0100

    la creatio enveronnement de projet

---
+++ Enaa-Fill-rouge/src/main/java/ma/enaa/enaafillrouge/EnaaFillRougeApplication.java
@@ -0,0 +1,13 @@
+package ma.enaa.enaafillrouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnaaFillRougeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnaaFillRougeApplication.class, args);
+    }
+
+}
---
+++ Enaa-Fill-rouge/src/test/java/ma/enaa/enaafillrouge/EnaaFillRougeApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enaa.enaafillrouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnaaFillRougeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
